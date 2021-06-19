538ec661c0982246c1fb9619d5b88fcc5da3f5b4
carolinahuergo <77100061+carolinahuergo@users.noreply.github.com>
2021-06-19 18:38:35 -0300

    Update Sala.java

--- src/Sala.java
+++ src/Sala.java
@@ -11,7 +11,6 @@ import javax.swing.SpinnerNumberModel;
 import javax.swing.JTextField;
 import javax.swing.event.ChangeListener;
 
-
 import javax.swing.event.ChangeEvent;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
@@ -23,53 +22,41 @@ public class Sala extends JPanel {
 	private JTextField txtJugador_3;
 	private JTextField txtJugador_4;
 	private JButton botonIniciarJuego = new JButton("COMENZAR JUEGO");
-	/**
-	 * Create the panel.
-	 */
- 
-    
+	private Integer cantJugadores;
+
 	public Sala() {
 		setBackground(new Color(238, 232, 170));
 		setForeground(Color.YELLOW);
 		setLayout(null);
-		
-		//JButton btnNewButton = new JButton("COMENZAR JUEGO");
-		
-//		btnNewButton.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//			}
-//		});
+
 		botonIniciarJuego.setBounds(273, 250, 146, 39);
 		add(botonIniciarJuego);
-		
+
 		JLabel lblNewLabel = new JLabel("Selecione la cantidad de jugadores : ( 2 - 4) ");
 		lblNewLabel.setBounds(10, 33, 242, 24);
 		add(lblNewLabel);
-		
+
+		this.cantJugadores = 2;
 		JSpinner spinner = new JSpinner();
 		spinner.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent arg0) {
-				
-				
-				if((Integer)spinner.getValue() == 2) {
-					txtJugador_3.setVisible(false);
-					txtJugador_4.setVisible(false);
-				}
-				
-				if((Integer)spinner.getValue() == 3) {
+
+				txtJugador_3.setVisible(false);
+				txtJugador_4.setVisible(false);
+
+				if ((Integer) spinner.getValue() >= 3)
 					txtJugador_3.setVisible(true);
-					txtJugador_4.setVisible(false);
-				}
-				if((Integer)spinner.getValue() == 4) {
-					txtJugador_3.setVisible(true);
+
+				if ((Integer) spinner.getValue() == 4)
 					txtJugador_4.setVisible(true);
-				}
+				
+				cargarVariableCantJugadores((Integer) spinner.getValue());
+
 			}
 		});
 		spinner.setModel(new SpinnerNumberModel(2, 2, 4, 1));
 		spinner.setBounds(374, 35, 45, 20);
 		add(spinner);
-		
 
 		txtJugador_1 = new JTextField();
 		txtJugador_1.setText("Jugador 1");
@@ -82,7 +69,7 @@ public class Sala extends JPanel {
 				System.out.println(txtJugador_1.getText());
 			}
 		});
-		
+
 		txtJugador_2 = new JTextField();
 		txtJugador_2.setText("Jugador 2");
 		txtJugador_2.setColumns(10);
@@ -94,8 +81,7 @@ public class Sala extends JPanel {
 				System.out.println(txtJugador_2.getText());
 			}
 		});
-		
-		
+
 		txtJugador_3 = new JTextField();
 		txtJugador_3.setText("Jugador 3");
 		txtJugador_3.setVisible(false);
@@ -108,8 +94,7 @@ public class Sala extends JPanel {
 				System.out.println(txtJugador_3.getText());
 			}
 		});
-		
-		
+
 		txtJugador_4 = new JTextField();
 		txtJugador_4.setText("Jugador 4");
 		txtJugador_4.setVisible(false);
@@ -122,13 +107,19 @@ public class Sala extends JPanel {
 				System.out.println(txtJugador_4.getText());
 			}
 		});
-		
-		
-	    }
-	
+
+	}
+
 	public void botonIniciarJuegoActionListener(ActionListener listener) {
 		botonIniciarJuego.addActionListener(listener);
 	}
+
+	public Integer getCantJugadores() {
+		return cantJugadores;
+	}
 	
+	public void cargarVariableCantJugadores(Integer cant) {
+		this.cantJugadores= cant;
 	}
 
+}
