7f9c8439c56441b6062ea3f60298b6ee66658d00
carolinahuergo <77100061+carolinahuergo@users.noreply.github.com>
2021-06-19 18:37:44 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,23 +1,16 @@
-import java.applet.AudioClip;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
 
 public class Principal extends JFrame implements ActionListener {
 
-	private Sala sala = new Sala();
-	private Juego juego = new Juego();
-	/**
-	 * Launch the application.
-	 */
+	private Sala sala;
+	private Juego juego;
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -32,40 +25,36 @@ public class Principal extends JFrame implements ActionListener {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public Principal() {
 		
-	
+		this.sala = new Sala();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1920, 1080);
-		//sala = new Sala();
 		sala.setBorder(new EmptyBorder(5, 5, 5, 5));
-		//setContentPane(sala);
 		sala.setLayout(null);
 		add(sala,BorderLayout.CENTER);
 		sala.botonIniciarJuegoActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				setContentPane(juego);
-				//pack();
+				crearJuego();
 			}
 		});
 		
-		//juego = new Juego();
-		juego.setBorder(new EmptyBorder(5, 5, 5, 5));
-		juego.setLayout(new BorderLayout(0, 0));
-		add(juego,BorderLayout.CENTER);
 		setContentPane(sala);
-		
-		//pack();
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		
 		setContentPane(juego);
-		
+	}
+	
+	public void crearJuego() {
+		this.juego = new Juego(sala.getCantJugadores());
+		juego.crearJuego();
+		juego.setBorder(new EmptyBorder(5, 5, 5, 5));
+		juego.setLayout(new BorderLayout(0, 0));
+		add(juego,BorderLayout.CENTER);
+		setContentPane(juego);	
 	}
 }
