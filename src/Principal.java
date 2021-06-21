c4aba302fbd7f18853498a5110fe33976bdb4c20
Tomas Serravento <81922863+tomasserravento@users.noreply.github.com>
2021-06-21 01:14:09 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.border.EmptyBorder;
 
 
@@ -55,6 +59,13 @@ public class Principal extends JFrame implements ActionListener {
 		juego.setBorder(new EmptyBorder(5, 5, 5, 5));
 		juego.setLayout(new BorderLayout(0, 0));
 		add(juego,BorderLayout.CENTER);
-		setContentPane(juego);	
+		setContentPane(juego);
+		
+		ImageIcon backgroundImg=new ImageIcon(getClass().getResource("fondoMadera.jpg"));
+		Image img=backgroundImg.getImage();
+		Image newimg=img.getScaledInstance(getWidth(),getHeight(),Image.SCALE_SMOOTH);
+		backgroundImg = new ImageIcon(newimg);
+		JLabel background = new JLabel(backgroundImg);
+		add(background);
 	}
 }
